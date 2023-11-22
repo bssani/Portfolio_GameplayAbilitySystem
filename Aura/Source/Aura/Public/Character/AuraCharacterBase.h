cd8a65d936ea6bbe26d90e1d3a118531c97a2212
bssani <vlfflq48@naver.com>
2023-11-22 10:35:21 +0900

    Character Blueprint Setup

--- Aura/Source/Aura/Public/Character/AuraCharacterBase.h
+++ Aura/Source/Aura/Public/Character/AuraCharacterBase.h
@@ -17,6 +17,13 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	//SkeletalMesh Component pointer 대신 TObjectPtr 사용
+	//패키지 파일에서는 같으나, 에디터에서 추가 기능이 있다. 
+	// Access Tracking and Optional Lazy Load Behavior
+	// Access Tracking - 얼마나 자주 포인터가 참조되거나 역참조되는지 트래킹할 수 있다.
+	// Lazy Loading - 에셋이 프로그램에 필요하거나 사용되기 전에는 로드 되지 않는다.
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
 
 
 };
